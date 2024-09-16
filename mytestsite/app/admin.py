dd23e2720bb86a8c5bdfb08562350a766795ad2a
Yaelle PIHAN <ypihan@IFREMER.FR>
2024-09-16 14:59:35 +0200

    debug en cours

--- mytestsite/app/admin.py
+++ mytestsite/app/admin.py
@@ -22,8 +22,20 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
 # Register the Admin classes for BookInstance using the decorator
+
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-     list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
 
 

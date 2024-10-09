60f74ddfc385269bd1ef86e80f64d2bb871fb6e8
Yaelle PIHAN <ypihan@IFREMER.FR>
2024-10-09 18:02:29 +0200

    table research

--- mytestsite/mytestsite/settings.py
+++ mytestsite/mytestsite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     'app.apps.AppConfig', # This object was created for us in /app/apps.py
 ]
 
